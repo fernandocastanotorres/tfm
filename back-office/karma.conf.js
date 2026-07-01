6dc19911bea2b3df870c7db0092ca19be3bb1e19
fcastano <fernando.castanotorres.ext@telefonica.com>
2026-07-01 15:18:40 +0200

    fix: add coverage to karma reporters array (Angular 20 CLI not injecting it)

--- back-office/karma.conf.js
+++ back-office/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
 
       ],
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
