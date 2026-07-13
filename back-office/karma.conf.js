53374492a523dfb723dff7636dab0019ec9df451
fcastano <fernando.castanotorres.ext@telefonica.com>
2026-07-13 16:00:57 +0200

    fix: remove kjhtml reporter to avoid DOM manipulation conflicts

--- back-office/karma.conf.js
+++ back-office/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
 
       ],
     },
-    reporters: ['progress', 'kjhtml', 'coverage'],
+    reporters: ['progress', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
